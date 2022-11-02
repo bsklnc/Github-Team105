0e1ccc0a46f339662c926d9b89d42ddc940e1ba2
bsklnc <115625704+bsklnc@users.noreply.github.com>
2022-11-02 14:45:17 +0300

    first commit

--- src/A01.java
+++ src/A01.java
@@ -2,6 +2,5 @@ public class A01 {
     public static void main(String[] args) {
         System.out.println("Hello github");
 
-
     }
 }
--- src/A04.java
+++
@@ -1,5 +0,0 @@
-public class A04 {
-    public static void main(String[] args) {
-
-    }
-}
