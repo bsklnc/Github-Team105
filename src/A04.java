b96c2fb9c661792bff2180125df77765480a043f
bsklnc <115625704+bsklnc@users.noreply.github.com>
2022-11-02 15:17:49 +0300

    aciklamalar eklendi

--- src/A04.java
+++ src/A04.java
@@ -1,5 +1,20 @@
 public class A04 {
     public static void main(String[] args) {
 
+       /*
+       bir projeyi ilk defa olusturdugumuzda
+        1-commit olustur
+        2-uzak masaüstünde bir repo olustur
+        3-github da istenen son 3 satiri copy yapip
+        projeye paste yapip calistir
+
+        Bir projeyi bir kere uzak masaüstü ile iliskilendirince
+       sonra yapacagimiz her degisiklik icin
+       1- git add .  : yeni yapilan degisikleri git e ekler
+       2- git commit -m"istenen acıklama"  : degisikleri isleyerek versiyon olusturur
+       3- git push : son versiyonu uzak masaüstüne yollar
+
+
+     */
     }
 }
